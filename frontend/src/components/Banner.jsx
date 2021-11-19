bdde50905064954004008ef106fd3b0d1720c57c
khanhhungvu1508 <khanhhungvu1508@gmail.com>
2021-11-19 09:54:42 +0700

    Merge HomePage and Menu Category together

---
+++ frontend/src/components/Banner.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './myBanner.css';
+
+class Banner extends React.Component {
+    render() {
+        return (
+            <>
+                <div class ="container" className ="page-banner-section">
+                    <h1> Our Menu</h1>
+                    <span>  Không ngon không lấy tiền </span>
+                </div>
+            </>
+        )
+    }
+}
+
+export default Banner;
---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { Navbar, Container, Nav, NavDropdown, Form, FormControl, Button } from "react-bootstrap";
+// import { useEffect } from 'react';
+// import './myBanner.css';
+
+const category = {
+  rice: "Món cơm",
+  dessert: "Món tráng miệng",
+  pho: "Món phở",
+  noodle: "Món mì",
+  stickyrice: "Món xôi",
+  drink: "Thức uống"
+}
+
+class MyNavbar extends React.Component {
+    render(){
+        return(
+          <>
+          <Navbar bg="light" expand="lg" fixed="top">
+            <Container fluid>
+              <Navbar.Brand href="#">
+                <img src ="https://i.imgur.com/3AUAOKZ.jpg" alt="" width="40" height = "35"></img>
+                <span>HyperWindy</span>
+              </Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                  <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                      className="me-auto my-2 my-lg-0"
+                      style={{ maxHeight: '100px', 
+                               padding: '0.25rem 0.75rem',
+                               margin: '0 auto', 
+                               fontFamily: 'inherit',
+                               fontSize: '20px',
+                               fontWeight: '500',
+                                 }}
+                      navbarScroll
+                    >
+                  <Nav.Link style={{margin: "0 25px" }} onClick = {() => this.props.selectPage("home")}>Home</Nav.Link>
+                  <NavDropdown title="Menu" id="navbarScrollingDropdown"  style={{margin: "0 25px" }}>
+                  <NavDropdown.Item onClick = {() => this.props.changeCategory(category.rice)}>Món cơm</NavDropdown.Item>
+                  <NavDropdown.Item onClick = {() => this.props.changeCategory(category.pho)}>Món phở</NavDropdown.Item>
+                  <NavDropdown.Item onClick = {() => this.props.changeCategory(category.stickyrice)}>Món xôi</NavDropdown.Item>
+                  <NavDropdown.Item onClick = {() => this.props.changeCategory(category.noodle)}>Món mì</NavDropdown.Item>
+                  <NavDropdown.Item onClick = {() => this.props.changeCategory(category.dessert)}>Món tráng miệng</NavDropdown.Item>
+                  <NavDropdown.Item onClick = {() => this.props.changeCategory(category.drink)}>Thức uống</NavDropdown.Item>
+                
+          {/* <NavDropdown.Divider />
+          <NavDropdown.Item href="#action5">
+            Something else here */}
+          {/* </NavDropdown.Item> */}
+                  </NavDropdown>
+                  <Nav.Link href="#action6" style={{margin: "0 25px" }} onClick = {() => this.props.selectPage("about")}>
+                    About
+                  </Nav.Link>
+                  <Nav.Link href="#action7" style={{margin: "0 25px" }} onClick = {() => this.props.selectPage("contact")}>
+                    Contact
+                  </Nav.Link>
+                </Nav>
+              <Form className="d-flex">
+                <FormControl
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+              <Button variant="outline-success">Search</Button>
+              </Form>
+            </Navbar.Collapse>
+          </Container>
+          
+        </Navbar>
+
+        
+        </>
+        );
+    }
+}
+export default MyNavbar;
